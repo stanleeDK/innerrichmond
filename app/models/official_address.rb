e0565a60a6e7400c5cf2ed06754714508f5d4e5a
Stan Lee <stan@buildzoom.com>
2017-08-04 09:12:04 -0700

    prodfix 2 pgsearch

--- app/models/official_address.rb
+++ app/models/official_address.rb
@@ -1,6 +1,6 @@
 class OfficialAddress < ActiveRecord::Base
-	include PgSearch
-	pg_search_scope :search_official_addresses, against: [:address]
+	# include PgSearch
+	# pg_search_scope :search_official_addresses, against: [:address]
 		# ,
 		# using: {
 		# 	tsearch: { prefix: true}
